d57d197ddc8e53d5bbeaa9035ac04a69e9a0e6b3
Rishi Tiwari <umesh896073@gmail.com>
2025-08-08 22:12:23 +0530

    final commit

--- build.sh
+++ build.sh
@@ -6,8 +6,11 @@ npm run build
 # Ensure routing works by copying index.html to 200.html
 cp build/index.html build/200.html
 
-# Copy static.json to the build directory
+# Copy configuration files to the build directory
 cp public/static.json build/
+cp public/_redirects build/
+cp public/render.json build/
+cp public/spa-router.json build/
 
 # Copy _redirects to the build directory
 cp public/_redirects build/
